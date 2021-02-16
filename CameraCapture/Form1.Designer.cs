780a9a96e58f2920b49cc51f60867c5a44df22fe
António Daniel <antoniocordeiro228@gmail.com>
2021-02-16 18:59:54 +0100

    Add project files.

---
+++ CameraCapture/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CameraCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(349, 187);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(684, 232);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 232);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(410, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(349, 187);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(410, 232);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(75, 23);
+            this.btnCapture.TabIndex = 4;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 301);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1039, 183);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 516);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CameraCapture/Form1.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WinFormCharpWebCam;
+using System.Data.SqlClient;
+
+namespace CameraCapture
+{
+    public partial class Form1 : Form
+    {
+        WebCam webCam;
+        string ConnectionString = "Data Source=DESKTOP-TSH8ILJ;Initial Catalog=bd_photo;Integrated Security=True;User ID=sagres_user;Password=sagres_user";
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(this.ConnectionString);
+        }
+  
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            webCam = new WebCam();
+            webCam.InitializeWebCam(ref pictureBox1);
+            webCam.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {          
+            try
+            {
+
+                connection.Open();
+                SqlCommand cmd = new SqlCommand($"Select photo from photos order by id desc", connection);
+                byte[] image = (byte[])cmd.ExecuteScalar();
+                if (image != null)
+                {
+
+                    MemoryStream ms = new MemoryStream(image);
+                    pictureBox2.Image = Image.FromStream(ms);
+                }
+
+                connection.Close();
+            }
+            catch (SqlException exception1)
+            {
+                SqlException ex = exception1;
+                SqlException exception = ex;
+                string caption = "Ocorreu o seguinte erro: " + exception.Number.ToString();
+                MessageBox.Show(exception.Message, caption);
+            }
+            
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+           
+            pictureBox2.Image = pictureBox1.Image;
+            webCam.Stop();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                connection.Open();
+                string QueryString = $"INSERT INTO photos (titlte, photo) VALUES (@title, @photo)";
+                SqlCommand command = new SqlCommand(QueryString, connection);
+
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@title", "Capture photografer");
+
+                MemoryStream stream = new MemoryStream();
+                pictureBox2.Image.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] pic = stream.ToArray();
+
+                command.Parameters.AddWithValue("@photo", pic);
+                
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("data saved");
+            }
+            catch (SqlException exception1)
+            {
+                SqlException ex = exception1;
+                SqlException exception = ex;
+                string caption = "Ocorreu o seguinte erro: " + exception.Number.ToString();
+                MessageBox.Show(exception.Message, caption);
+            }
+        }
+    }
+}
